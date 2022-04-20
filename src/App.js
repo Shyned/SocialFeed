51a410738aadd4992b1bf0e516c317077b73a41b
Dyshine Hill <eyedolarts@gmail.com>
2022-04-20 13:54:00 -0500

    created Name and Post fields in App.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react';
+
 
 function App() {
+  const [post, setpost] = useState([{name:'jj vega',post:'Its April Fools Day! Give this a dislike if you really like it. : )'},{name:'Nevin Siebal',post:"My Rubik's cube is fully charged and ready for battle. Right after I eat my meal that has appeared from the magical hand from behind my curtain."}])
+  
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <form>
+       <label>NAME
+         <input></input>
+       </label>
+       <label>POST
+         <input></input>
+       </label>
+     </form>
+   </div>
   );
 }
 
